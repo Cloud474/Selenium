da50428651631cc8e38550b42da4e540676e3342
Cloud474 <huseyin.blt47@hotmail.com>
2022-09-17 11:21:33 +0300

    changes

---
+++ src/Testing_Tool/TestingTool_1/Soru_5/Soru5.java
@@ -0,0 +1,26 @@
+package Testing_Tool.TestingTool_1.Soru_5;
+
+import Utils.BaseStaticDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Soru5 extends BaseStaticDriver {
+    public static void main(String[] args) {
+
+        driver.get("https://testpages.herokuapp.com/styled/index.html");
+        WebElement btn=driver.findElement(By.id("fakealerttest"));
+        btn.click();
+        WebElement btn2=driver.findElement(By.id("fakealert"));
+        btn2.click();
+        WebElement btn3=driver.findElement(By.id("dialog-ok"));
+        btn3.click();
+        WebElement dlg=driver.findElement(By.id("dialog-text"));
+        if (dlg.getText().contains("fake"))
+            System.out.println("Test Failed");
+        else
+            System.out.println("Test Passed");
+
+        BekleKapat();
+
+    }
+}
---
+++ src/Testing_Tool/TestingTool_2/Soru2/Soru2.java
@@ -0,0 +1,25 @@
+package Testing_Tool.TestingTool_2.Soru2;
+
+import Utils.BaseStaticDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Soru2 extends BaseStaticDriver {
+    public static void main(String[] args) {
+        driver.get("https://demo.applitools.com/");
+        Bekle(2);
+        WebElement user=driver.findElement(By.cssSelector("[class='form-group']>input"));
+        user.sendKeys("ttechno@gmail.com");
+        WebElement pass=driver.findElement(By.cssSelector("[id='password'][class='form-control']"));
+        pass.sendKeys("techno123.");
+        WebElement sign=driver.findElement(By.cssSelector("[class='buttons-w']>a"));
+        sign.click();
+        WebElement msg= driver.findElement(By.cssSelector("[class='content-box']>div h6"));
+        if (msg.getText().contains("branch"))
+            System.out.println("Test Passed");
+        else
+            System.out.println("Test Failed");
+
+        BekleKapat();
+    }
+}
